757dd20dd8612edc6366884c749d74c09b673cee
UwUSnek <thatsnekuwu@gmail.com>
2024-02-15 19:57:55 +0100

    Fix BlockGradient generator

--- src/main/java/org/shadownight/plugin/shadownight/dungeons/shaders/SHD_WallMaterial.java
+++ src/main/java/org/shadownight/plugin/shadownight/dungeons/shaders/SHD_WallMaterial.java
@@ -11,19 +11,19 @@ public class SHD_WallMaterial extends Rnd {
     static private final BlockGradient patternWall = new BlockGradient(
         Material.DEEPSLATE_TILES,
         Material.COBBLED_DEEPSLATE,
-        Material.COBBLESTONE
+        Material.COBBLESTONE,
+        Material.ANDESITE
     );
 
 
 
 
     private static Material compute(int x, int y, int z, int wallHeight, int floorThickness) {
-        float n = (float)(y - floorThickness) / wallHeight;
-        return patternWall.get(n);
+        return patternWall.get((float)(y - floorThickness) / wallHeight);
     }
 
     public static void start(RegionBuffer buffer, Material material, int wallHeight, int floorThickness) {
-        for(int i = 0; i < buffer.x; ++i) for(int j = 0; j < buffer.y; ++j) for(int k = 0; k < buffer.z; ++k){
+        for(int i = 0; i < buffer.x; ++i) for(int j = floorThickness; j < buffer.y; ++j) for(int k = 0; k < buffer.z; ++k){
             if(buffer.get(i, j, k) == material) buffer.set(i, j, k, compute(i, j, k, wallHeight, floorThickness));
         }
     }
