4dceb8d3b2cf93aa293d86ab25cc45a3fbaafcf0
UwUSnek <thatsnekuwu@gmail.com>
2024-02-20 12:17:34 +0100

    Minor improvements

---
+++ src/main/java/org/shadownight/plugin/shadownight/dungeons/debug/SHD_VisualizePerlinNoise2D.java
@@ -0,0 +1,28 @@
+package org.shadownight.plugin.shadownight.dungeons.debug;
+
+
+import org.bukkit.Material;
+import org.shadownight.plugin.shadownight.dungeons.shaders.SHD;
+
+
+public final class SHD_VisualizePerlinNoise2D extends SHD {
+    final int gridSize;
+    final int yLevel;
+
+    /**
+     * @param _gridSize The normals of each block
+     * @param _yLevel The y level in which to draw the perlin noise slice
+     */
+    public SHD_VisualizePerlinNoise2D(final int _gridSize, final int _yLevel) {
+        gridSize = _gridSize;
+        yLevel = _yLevel;
+    }
+
+    @Override
+    public void compute(final int x, final int y, final int z) {
+        if(y == yLevel) {
+            o.set(x, yLevel, z, Material.WHITE_STAINED_GLASS.createBlockData());
+            o.set(x, yLevel + (int)(perlinNoise2D.compute(x, z, gridSize) * ((float)gridSize / 2)), z, Material.WHITE_CONCRETE.createBlockData());
+        }
+    }
+}
