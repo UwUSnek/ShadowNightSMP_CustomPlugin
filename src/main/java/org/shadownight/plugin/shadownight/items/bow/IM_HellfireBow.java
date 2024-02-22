718288e879f589888296f4e602985d8bb210b411
UwUSnek <thatsnekuwu@gmail.com>
2024-02-22 02:20:26 +0100

    Add BlockProperty utility class

--- src/main/java/org/shadownight/plugin/shadownight/items/bow/IM_HellfireBow.java
+++ src/main/java/org/shadownight/plugin/shadownight/items/bow/IM_HellfireBow.java
@@ -1,9 +1,7 @@
 package org.shadownight.plugin.shadownight.items.bow;
 
 
-import org.bukkit.Bukkit;
-import org.bukkit.Location;
-import org.bukkit.Material;
+import org.bukkit.*;
 import org.bukkit.entity.Arrow;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityShootBowEvent;
@@ -12,14 +10,20 @@ import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.ShapedRecipe;
 import org.bukkit.util.Vector;
+import org.javatuples.Pair;
 import org.jetbrains.annotations.NotNull;
 import org.shadownight.plugin.shadownight.ShadowNight;
 import org.shadownight.plugin.shadownight.items.CustomItemId;
+import org.shadownight.plugin.shadownight.utils.blockdata.BlockProperty;
 import org.shadownight.plugin.shadownight.utils.math.Easing;
 import org.shadownight.plugin.shadownight.utils.math.Func;
+import org.shadownight.plugin.shadownight.utils.utils;
 
 import java.util.Objects;
 import java.util.logging.Level;
+
+
+
 
 public final class IM_HellfireBow extends IM_Bow {
     @Override public Material getMaterial()            { return Material.BOW;              }
@@ -42,32 +46,58 @@ public final class IM_HellfireBow extends IM_Bow {
     }
 
 
+
+
     @Override
     protected void onProjectileHit(@NotNull final ProjectileHitEvent event, @NotNull final ItemStack usedBow) {
         // (3 is the maximum initial vanilla speed when shot by a player)
         final double maxSpeed = 3.0; // The maximum speed of the arrow. This limit prevents exploits using tnt knockback on custom bow arrows
         final double minSpeed = 0.5; // The minimum speed of the arrow. Doesn't actually speed it up but limits the affected hit area length
 
+        // Get precise initial location
+        final Location initialPos;
+        /**/ if(event.getHitEntity() != null) initialPos = event.getHitEntity().getLocation().getBlock().getLocation();
+        else if(event.getHitBlock()  != null) initialPos = event.getHitBlock().getLocation();
+        else return;
+
+        // Calculate velocity and direction
         final Arrow e = (Arrow)event.getEntity();
         final Vector dir = e.getVelocity().multiply(new Vector(1, 0, 1)).normalize();
         final Vector side = dir.clone().rotateAroundY(Math.PI / 2);
-
         final double width  = Func.clamp(e.getVelocity().length(), 1,        maxSpeed);     // Min 1, Max 3
         final double length = Func.clamp(e.getVelocity().length(), minSpeed, maxSpeed) * 10; // Min 4, Max 30
-        loop(0, 0.05, e.getLocation(), length, width, dir, side);
-        loop(0, 0.05, e.getLocation(), length, width, dir.clone().rotateAroundY(Math.PI /  8), side.clone().rotateAroundY(Math.PI /  8));
-        loop(0, 0.05, e.getLocation(), length, width, dir.clone().rotateAroundY(Math.PI / -8), side.clone().rotateAroundY(Math.PI / -8));
+
+        // Start animation
+        loop(0, 0.05, initialPos, length, width, dir, side);
+        loop(0, 0.05, initialPos, length, width, dir.clone().rotateAroundY(Math.PI /  8), side.clone().rotateAroundY(Math.PI /  8));
+        loop(0, 0.05, initialPos, length, width, dir.clone().rotateAroundY(Math.PI / -8), side.clone().rotateAroundY(Math.PI / -8));
     }
 
 
+
+
+    private boolean isTargetValid(@NotNull final World w, @NotNull final Location target) {
+        return
+            BlockProperty.isDelicate(w.getBlockAt(target).getType()) &&
+            !BlockProperty.isDelicate(w.getBlockAt(target.clone().subtract(0, 1, 0)).getType())
+        ;
+    }
+
     private void loop(final double from, final double to, @NotNull final Location startLocation, final double length, final double width, @NotNull final Vector dir, @NotNull final Vector side) {
-        for (double i = Easing.cubicOut(from) * length; i < Easing.cubicOut(to) * length; i += 0.25f) {
-            for(double j = -width; j < width; j += 0.5f) {
-                Objects.requireNonNull(startLocation.getWorld()).getBlockAt(
+        final World w = startLocation.getWorld();
+        for(double k = -width; k < width; k += 0.5f) {
+            for (double i = Easing.cubicOut(from) * length; i < Easing.cubicOut(to) * length; i += 0.25f) {
+                final Location target = (
                     startLocation.clone()
                     .add(dir.clone().multiply(i))
-                    .add(side.clone().multiply(j))
-                ).setType(Material.MAGMA_BLOCK);
+                    .add(side.clone().multiply(k))
+                );
+                /**/ if(isTargetValid(w, target                  )) w.getBlockAt(target).setType(Material.FIRE);
+                //else if(isTargetValid(w, target.add     (0, 1, 0))) { w.getBlockAt(target).setType(Material.FIRE);
+                //    utils.log(Level.WARNING, "TARGET +1");}
+                //else if(isTargetValid(w, target.subtract(0, 2, 0))) { w.getBlockAt(target).setType(Material.FIRE);
+                //    utils.log(Level.WARNING, "TARGET -1");}
+                else break; // If no valid spot is found, stop the current stripe
             }
         }
         if(to < 1) Bukkit.getScheduler().runTaskLater(ShadowNight.plugin, () ->
---
+++ src/main/java/org/shadownight/plugin/shadownight/utils/blockdata/BlockProperty.java
@@ -0,0 +1,137 @@
+package org.shadownight.plugin.shadownight.utils.blockdata;
+
+
+import org.bukkit.Material;
+import org.bukkit.Tag;
+import org.jetbrains.annotations.NotNull;
+import org.shadownight.plugin.shadownight.utils.UtilityClass;
+
+
+
+
+public final class BlockProperty extends UtilityClass {
+    /**
+     * Identifies blocks that can be broken and/or replaced by destructive abilities.
+     * @param material The material of the block
+     * @return True if the block can be destroyed, false otherwise
+     */
+    public static boolean isDelicate(@NotNull final Material material) {
+        return
+            Tag.REPLACEABLE      .isTagged(material) ||
+            Tag.CORAL_PLANTS     .isTagged(material) ||
+            Tag.WALL_CORALS      .isTagged(material) ||
+            Tag.CROPS            .isTagged(material) ||
+            Tag.WOOL_CARPETS     .isTagged(material) || material == Material.MOSS_CARPET ||
+            Tag.FLOWER_POTS      .isTagged(material) ||
+            Tag.FLOWERS          .isTagged(material) ||
+            Tag.PRESSURE_PLATES  .isTagged(material) ||
+            Tag.RAILS            .isTagged(material) ||
+            Tag.SAPLINGS         .isTagged(material) ||
+            Tag.ALL_SIGNS        .isTagged(material) ||
+            Tag.ALL_HANGING_SIGNS.isTagged(material) ||
+            Tag.BUTTONS          .isTagged(material) ||
+            Tag.CANDLES          .isTagged(material) ||
+
+            material == Material.CAVE_AIR ||
+            material == Material.VOID_AIR ||
+            material == Material.WATER ||
+            material == Material.LAVA ||
+            material == Material.SNOW ||
+            material == Material.SCULK_VEIN ||
+
+            material == Material.WEEPING_VINES ||
+            material == Material.TWISTING_VINES ||
+            material == Material.KELP ||
+            material == Material.WEEPING_VINES_PLANT ||
+            material == Material.TWISTING_VINES_PLANT ||
+            material == Material.KELP_PLANT ||
+            material == Material.BROWN_MUSHROOM ||
+            material == Material.RED_MUSHROOM ||
+            material == Material.CRIMSON_FUNGUS ||
+            material == Material.WARPED_FUNGUS ||
+            material == Material.SMALL_DRIPLEAF ||
+            material == Material.BIG_DRIPLEAF ||
+            material == Material.SWEET_BERRY_BUSH ||
+            material == Material.LILY_PAD ||
+
+            material == Material.TORCH ||
+            material == Material.SOUL_TORCH ||
+            material == Material.WALL_TORCH ||
+            material == Material.SOUL_WALL_TORCH ||
+
+            material == Material.STRING ||
+            material == Material.TURTLE_EGG ||
+            material == Material.FROGSPAWN
+        ;
+    }
+
+
+    /**
+     * Identifies blocks that can be walked through without the need to break them, sneak or crouch.
+     * @param material The material of the block
+     * @return True if the block is walkable, false otherwise
+     */
+    public static boolean isWalkable(@NotNull final Material material) {
+        return
+            Tag.REPLACEABLE      .isTagged(material) ||
+            Tag.CORAL_PLANTS     .isTagged(material) ||
+            Tag.WALL_CORALS      .isTagged(material) ||
+            Tag.CROPS            .isTagged(material) ||
+            Tag.WOOL_CARPETS     .isTagged(material) || material == Material.MOSS_CARPET ||
+            Tag.FLOWER_POTS      .isTagged(material) ||
+            Tag.FLOWERS          .isTagged(material) ||
+            Tag.PRESSURE_PLATES  .isTagged(material) ||
+            Tag.RAILS            .isTagged(material) ||
+            Tag.SAPLINGS         .isTagged(material) ||
+            Tag.ALL_SIGNS        .isTagged(material) ||
+            Tag.ALL_HANGING_SIGNS.isTagged(material) ||
+            Tag.BUTTONS          .isTagged(material) ||
+            Tag.CANDLES          .isTagged(material) ||
+
+            material == Material.CAVE_AIR ||
+            material == Material.VOID_AIR ||
+            material == Material.WATER ||
+            material == Material.LAVA ||
+            material == Material.SNOW ||
+            material == Material.SCULK_VEIN ||
+
+            material == Material.WEEPING_VINES ||
+            material == Material.TWISTING_VINES ||
+            material == Material.KELP ||
+            material == Material.WEEPING_VINES_PLANT ||
+            material == Material.TWISTING_VINES_PLANT ||
+            material == Material.KELP_PLANT ||
+            material == Material.BROWN_MUSHROOM ||
+            material == Material.RED_MUSHROOM ||
+            material == Material.CRIMSON_FUNGUS ||
+            material == Material.WARPED_FUNGUS ||
+            material == Material.SMALL_DRIPLEAF ||
+            material == Material.BIG_DRIPLEAF ||
+            material == Material.SWEET_BERRY_BUSH ||
+            material == Material.LILY_PAD ||
+
+            material == Material.REDSTONE_WIRE ||
+            material == Material.REDSTONE_TORCH ||
+            material == Material.TORCH ||
+            material == Material.SOUL_TORCH ||
+            material == Material.REDSTONE_WALL_TORCH ||
+            material == Material.WALL_TORCH ||
+            material == Material.SOUL_WALL_TORCH ||
+
+            material == Material.LADDER ||
+            material == Material.STRING ||
+            material == Material.TRIPWIRE_HOOK ||
+            material == Material.TURTLE_EGG ||
+            material == Material.FROGSPAWN ||
+            material == Material.AMETHYST_CLUSTER ||
+            material == Material.SMALL_AMETHYST_BUD ||
+            material == Material.MEDIUM_AMETHYST_BUD ||
+            material == Material.LARGE_AMETHYST_BUD ||
+
+            material == Material.LEVER ||
+            material == Material.REPEATER ||
+            material == Material.COMPARATOR ||
+            material == Material.LIGHT
+        ;
+    }
+}
