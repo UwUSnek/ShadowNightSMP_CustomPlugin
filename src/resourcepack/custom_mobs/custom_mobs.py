1a5ebf7da0bbac01d7b074955727bd63b304f5ed
UwUSnek <thatsnekuwu@gmail.com>
2024-03-13 05:02:35 +0100

    Add smooth font to resource pack

--- src/resourcepack/custom_mobs/custom_mobs.py
+++ src/resourcepack/custom_mobs/custom_mobs.py
@@ -1,2 +1,36 @@
+import os
+import json
 
 data_zero = 20_000
+
+models = [
+    { "id" : "test" }
+]
+
+
+
+source_base     = "./base_pack/assets/minecraft/models/item";         os.makedirs(source_base,     exist_ok=True)
+target_base     = "./build/output/assets/minecraft/models/item";      os.makedirs(target_base,     exist_ok=True)
+target_java     = "../main/java/org/uwu_snek/shadownight/_generated"; os.makedirs(target_java,     exist_ok=True)
+
+
+
+
+base = json.load(open(source_base + "/" + "bone.json", "r"))
+
+
+
+
+base["overrides"] = []
+#for i, model in enumerate(models):
+#    base["overrides"] += [{
+#        "predicate": {
+#            "custom_model_data": data_zero + i
+#        },
+#        "model": "shadow_night:item/" + model_name
+#    }]
+
+
+
+
+json.dump(base, open(target_base + "/" + "bone.json", "w+"), indent=4)
